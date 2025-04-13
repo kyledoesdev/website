80290eae1eed65255346c32d4d1df7d452d9b76c
kyledoesdev <kyledoesdev@gmail.com>
2025-04-13 19:25:09 -0400

    progress stash

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
+import path from 'path'
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import { watchAndRun } from 'vite-plugin-watch-and-run'
+import tailwindcss from "@tailwindcss/vite";
 import prismjs from 'vite-plugin-prismjs';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: [
                 'resources/css/app.css',
@@ -16,6 +20,15 @@ export default defineConfig({
             languages: ['php', 'javascript', 'html', 'css', 'bash', 'typescript'],
             theme: 'tomorrow',
             css: true,
-        })
+        }),
+        watchAndRun([
+            {
+                name: 'prezet:index',
+                watch: path.resolve('prezet/**/*.(md|jpg|png|webp)'),
+                run: 'php artisan prezet:index',
+                delay: 1000,
+                // watchKind: ['add', 'change', 'unlink'], // (default)
+            }
+        ])
     ],
 });
