8d6b1d4ee6806004c81f894ecd9fe0cc91302b1d
kyledoesdev <kyledoesdev@gmail.com>
2025-05-31 13:09:07 -0400

    fix tests

--- tests/Feature/PageTest.php
+++ tests/Feature/PageTest.php
@@ -1,37 +1,18 @@
 <?php
 
-it('can load welcome page', function () {
-    $response = $this->get('/');
+use App\Models\Asset;
 
-    $response->assertStatus(200);
-});
+it('can load public pages', function () {
+    Asset::factory()->resume()->create();
 
-it('can load work history page', function () {
-    $response = $this->get(route('work_history'));
-
-    $response->assertStatus(200);
-});
-
-it('can load education page', function () {
-    $response = $this->get(route('education'));
-
-    $response->assertStatus(200);
-});
-
-it('can load projects page', function () {
-    $response = $this->get(route('projects'));
-
-    $response->assertStatus(200);
-});
-
-it('can load technology page', function () {
-    $response = $this->get(route('technologies'));
-
-    $response->assertStatus(200);
-});
-
-it('can load gallery', function () {
-    $response = $this->get(route('gallery'));
-
-    $response->assertStatus(200);
+    $this->get(route('welcome'))->assertOk();
+    $this->get(route('education'))->assertOk();
+    $this->get(route('projects'))->assertOk();
+    $this->get(route('technologies'))->assertOk();
+    $this->get(route('work_history'))->assertOk();
+    $this->get(route('board_games'))->assertOk();
+    $this->get(route('movies'))->assertOk();
+    $this->get(route('music'))->assertOk();
+    $this->get(route('video_games'))->assertOk();
+    $this->get(route('gallery'))->assertOk();
 });
